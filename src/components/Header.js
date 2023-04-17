16b92c09c9267658c8cbc2a1a130e62b5a4ea1f0
pullso <pullso.code@gmail.com>
2023-04-17 22:16:42 +0300

    update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,43 @@
-import React from 'react';
-import './Header.css'
-import {Menu as MenuIcon} from "@mui/icons-material";
-import SearchIcon from '@mui/icons-material/Search';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import AppsIcon from '@mui/icons-material/Apps';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import {Avatar, IconButton} from "@mui/material";
+import React from "react";
+import "./Header.css";
+import { Menu as MenuIcon } from "@mui/icons-material";
+import SearchIcon from "@mui/icons-material/Search";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import AppsIcon from "@mui/icons-material/Apps";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import { Avatar, IconButton } from "@mui/material";
 
 const Header = () => {
-  return (<div className="header">
-    <div className="header__left">
-      <IconButton>
-        <MenuIcon/>
-      </IconButton>
-      <img src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_2x_r5.png" alt=""/>
+  return (
+    <div className="header">
+      <div className="header__left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img
+          src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_2x_r5.png"
+          alt=""
+        />
+      </div>
+      <div
+        className="header__middle
+      "
+      >
+        <SearchIcon />
+        <input type="text" placeholder="Search email" />
+        <ArrowDropDownIcon />
+      </div>
+      <div className="header__right">
+        <IconButton>
+          <AppsIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsIcon />
+        </IconButton>
+        <Avatar />
+      </div>
     </div>
-    <div className="header__middle
-      ">
-      <SearchIcon/>
-      <input type="text" placeholder='Search email'/>
-      <ArrowDropDownIcon/>
-    </div>
-    <div className="header__right">
-      <IconButton>
-        <AppsIcon/>
-      </IconButton>
-      <IconButton>
-        <NotificationsIcon/>
-      </IconButton>
-      <Avatar/>
-    </div>
-  </div>);
+  );
 };
 
 export default Header;
